0a78abcfd2ebdb6240979cd7a01dbd61c9f22be3
kapil-malviya <122611483+kapil-malviya@users.noreply.github.com>
2023-08-09 00:20:56 +0530

    program to check whether the given mail id is valid or not

--- re-14-mail-id-validator.py
+++ re-14-mail-id-validator.py
@@ -4,15 +4,15 @@
 
 import re
 
-s = input('enter email id : ')
-m = re.fullmatch('\w[a-zA-Z0-9_.]*@gmail[.]com', s)
+string = input('enter email id : ')
+match = re.fullmatch('\w[a-zA-Z0-9_.]*@gmail[.]com', s)
 
-# m = re.fullmatch('\w[a-zA-Z0-9_.]*@(gmail|rediffmail)[.]com', s)
-# m = re.fullmatch('\w[a-zA-Z0-9_.]*@[a-z0-9]+[.]com', s)   =>> ___@tv9.com
-# m = re.fullmatch('\w[a-zA-Z0-9_.]*@[a-z0-9]+[.][a-z]+', s)   =>> ___@tv9.net
+# match = re.fullmatch('\w[a-zA-Z0-9_.]*@(gmail|rediffmail)[.]com', string)
+# match = re.fullmatch('\w[a-zA-Z0-9_.]*@[a-z0-9]+[.]com', string)   =>> ___@tv9.com
+# match = re.fullmatch('\w[a-zA-Z0-9_.]*@[a-z0-9]+[.][a-z]+', string)   =>> ___@tv9.net
 
 
-if m != None :
+if match != None :
 	print('Valid')
 else :
 	print('Invalid')
